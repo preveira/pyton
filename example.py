17f9257724afd7ea4d89948f6fcf428ad2a1d0bf
Patrick Kawika Reveira <patrick.k.reveira@gmail.com>
2025-02-09 21:28:05 -0800

    created file for ch5

--- example.py
+++ example.py
@@ -73,16 +73,3 @@
 
 # main()
 
-def is_triangle(leng1, leng2, leng3):
-  if leng1 > leng2 + leng3:
-    print('No')
-  else:
-    if leng2 > leng1 + leng3:
-      print('No')
-    else:
-      if leng3 > leng1 + leng2:
-        print('No')
-      else:
-        print('Yes')
-
-is_triangle(4, 5, 6)
