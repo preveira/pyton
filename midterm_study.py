39ac8359d84186e00a2cbea4b4e1eb63a7ebc644
Patrick Kawika Reveira <patrick.k.reveira@gmail.com>
2025-02-19 10:51:51 -0800

    completed programming 2 problem set mideterm pratice exam

--- midterm_study.py
+++ midterm_study.py
@@ -5,3 +5,15 @@ print(has_midpoint(4,6,8))
 print(has_midpoint(3,1,3))
 
 # code passed problem set and test
+import random
+
+def give_problems(n):
+  for i in range(n):
+    x = random.randint(1,12)
+    y = random.randint(1,12)
+    prompt = int(input(f"{x} x {y} = "))
+    if prompt == x * y:
+      print('Correct')
+    else:
+      print(f'Incorrect, the answer was {x*y}')
+give_problems(3)
