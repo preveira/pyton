39ccb3c59ac4489d854d88b48a4a4a7634ecb523
Patrick Kawika Reveira <patrick.k.reveira@gmail.com>
2025-02-26 14:55:04 -0800

    chapter 7 exercises incomplete

--- Chapters/ch_7.py
+++ Chapters/ch_7.py
@@ -1,1 +1,46 @@
-#created file for chapter 7 practice and homework
+def uses_none(word, forbidden):
+  for letter in word.lower():
+      if letter in forbidden.lower():
+        return False 
+
+  return True
+
+print(uses_none('banana', 'xyz'))
+print(uses_none('apple', 'efg'))
+
+def uses_only(word, available):
+  for letter in word.lower():
+    if letter not in available.lower():
+      return False
+  return True
+      
+print(uses_only('apple', 'apl'))
+print(uses_only('banana', 'ban'))
+
+def uses_all(word, required):
+  for letter in required.lower():
+    if letter not in word.lower():
+      return False
+  return True
+
+print(uses_all('banana', 'nab'))
+print(uses_all('apple', 'ipa'))
+
+
+#check if the word length is long enough
+#check if word contains only characters from (available) letters
+#check if word contains required letter
+#if all of these are True then return True
+def check_word(word, available, required):
+  if len(word) < 4:
+    return False
+  for letter in word.lower():
+    if letter not in available.lower():
+      return False
+  for letter in required.lower():
+    if letter not in word.lower():
+      return False
+  return True
+
+print(check_word('color', 'ACDLORT', 'R'))
+print(check_word('told', 'ACDLORT', 'R'))
