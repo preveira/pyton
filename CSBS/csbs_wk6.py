be06ac36ac2f04e91cf82bc0d29138bfd56495f3
Patrick Kawika Reveira <patrick.k.reveira@gmail.com>
2025-03-03 17:01:57 -0800

    completed problem 2 csbs

--- CSBS/csbs_wk6.py
+++ CSBS/csbs_wk6.py
@@ -7,3 +7,24 @@ def number_loops(n):
       print('.', end='') # print the trailing periods\
     print() #skip to the next line
 number_loops(5)
+
+def number_square(min, max):
+  for row in range(min, max + 1):
+    number_to_print = row
+    for col in range(max - min + 1):
+      if number_to_print > max:
+        number_to_print = min
+      print(number_to_print, end='')
+      number_to_print += 1
+    print()
+
+number_square(1, 5)
+
+def number_square2(min, max):
+  base = max - min + 1
+  for row in range(max - min + 1):
+    for col in range(max - min + 1):
+      print(min + (row + col) % base, end='')
+    print()
+
+number_square2(1, 5)
